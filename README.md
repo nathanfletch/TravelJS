9c6d13b33c7910e8c4aa3248e0110458c7bd3cb8
nathanfletch <64681377+nathanfletch@users.noreply.github.com>
2021-10-27 16:09:24 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# City Tour
+
+#### This site recommends a user a tour, movies and national parks from click "moods".
+
+#### _By Paige Tiedeman, Nathan Fletcher, Connor Hansen, Diego Serafico & Mark Chang_
+
+## Technologies Used
+
+* HTML
+* CSS
+* JavaScript
+* JQuery
+* Bootstrap
+* ESLint
+* Babel
+* Webpack
+* Popper
+* Package.json
+* [Triposo-API](https://www.triposo.com/api/documentation/20210615/)
+* [Movie-glu](https://developer.movieglu.com/v2/api-index/quick-start-guide/)
+* [Mapbbox](https://docs.mapbox.com/api/maps/static-images/)
+* [Recration Information Database API](https://ridb.recreation.gov/landing)
+
+
+## Description
+
+This application integrates four different API's to suggest a day trip with map locations based on the mood clicked. It also has an option for National Parks based on user search and local movie API that uses coordinates that are currently set in the movie.js file.
+
+## Setup/Installation Requirements
+
+<details>
+<summary>Setup & Installation Requirements</summary>
+
+* _Clone or download the zip file of this repository to your desktop_
+* _Open in a text editor_
+* _If you are using a Mac: open the package.json and find the scripts property, change the line "start": "npm run build & webpack-dev-server --open --mode development",
+to "start": "npm run build; webpack-dev-server --open --mode development"_
+* _Commit and push your .gitignore file to your repo_
+* _Run `$ npm install`_
+* _Check title on package.json and webpack file_
+* _Go to https://www.triposo.com/api/signup and generate an API Key_
+* _Type `$ touch .env` to create a document to save your API key as Account_Key={your key here} & token={your token here}_
+* _Go to https://ridb.recreation.gov/landing to generate an API Key_
+  - add API_KEY={Your-Api-Key} to the .env file.
+* _Add MAP_KEY=pk.eyJ1IjoibmF0aGFubWFwYm94ZGV2IiwiYSI6ImNrcHdkN2p2aDA0Z2oyeG9ieDZicTZudm4ifQ.tUypg6GRjhED2bic-GkFnw (this is a public test key) to the .env file.
+* _Go to https://developer.movieglu.com/ and generate an API Key_
+* _In your .env file add the MOVIES_AUTHORIZATION=(), MOVIES_CLIENT=(), and API_KEY=() for movies_
+* _Type `$ npm run build`, to build your environment_
+* _Type `$ npm run start`, to open in your browser_
+* _Click on any of the moods to get your result_
+* _Use the Nav Bar to go to different Sections_
+* _Type in a US City to find nearby Parks_
+* _Click the "See Local Movies" Button to show nearby theaters and showing_
+</details>
+
+## Known Bugs
+
+* _National Parks API only work for US cities_
+* _Movies is set to the US and specific coordinates and will not change_
+* _Pick a new mood does not allow for results to populate a second time_
+
+## License
+
+_[MIT](https://opensource.org/licenses/MIT)_  
+
+Copyright (c) 2021 Paige Tiedeman, Nathan Fletcher, Connor Hansen, Diego Serafico & Mark Chang
+
+## Contact Information
+
+_Paige Tiedeman @ github.com/paigetiedeman_  
+_Nathan Fletcher @ github.com/nathanfletch_  
+_Connor Hansen @ github.com/chansen1395_  
+_Diego Serafico @ github.com/DiegoSerafico_  
+_Mark Chang_  
---
+++ src/index.js
@@ -0,0 +1,212 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Default from './assets/images/default.jpg';
+import cityTour from './js/city-tour.js';
+import OutdoorsRec from "./js/outdoors.js";
+import { Movies } from "./js/movies";
+
+async function makeApiCall(city) {
+  const response = await cityTour.getTour(city);
+  displayResult(response.results[0].days[0].itinerary_items);
+}
+
+function displayResult(items) {
+  const pointsHtml = items
+    .map((item) => {
+      let imageUrl = Default;
+      if (item.poi.images.length > 0) {
+        imageUrl = item.poi.images[0].source_url;
+      }
+      return `<div class="col my-3">
+        <div class="card mx-auto h-100" style="width: 18rem;">
+          <img class="card-img-top tour-image" src=${imageUrl} alt="Card image cap">
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">${item.poi.name}</h5>
+            <p class="card-text">${item.description}</p>
+          </div>
+        </div>
+      </div>`;
+    })
+    .join("");
+  $("#container").hide();
+  $("#results-container").show();
+  $("#navBar").show();
+  $(".dayPlan").show();
+  $("#outdoors-div").show();
+  $(".movies").show();
+  $("#cityDisplay").empty();
+  $("#cityDisplay").append(pointsHtml);
+  const geo = createGeoJson(items);
+  $("#map-wrapper").append(
+    `<img src=https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/geojson(${geo})/${items[0].poi.coordinates.longitude},${items[0].poi.coordinates.latitude},12/600x600?access_token=${process.env.MAP_KEY}>`
+  );
+}
+
+
+async function outdoorsIdCall(citySearch) {
+  const response = await OutdoorsRec.getOutdoors();
+  const searchIds = await OutdoorsRec.getIds(citySearch);
+  let areaIds = [];
+  for (let id = 0; id < searchIds.RECDATA.length; id++) {
+    areaIds.push(searchIds.RECDATA[id].RecAreaID);
+  }
+  outdoorsApiCalls(response.RECDATA, areaIds);
+}
+
+async function outdoorsUrlApiCall(outdoorsURL) {
+  try {
+    const outdoorsLink = await OutdoorsRec.getOutdoorsURL(outdoorsURL);
+    let urlLink = outdoorsLink.RECDATA[0].URL;
+    return urlLink;
+  }
+  catch (error) {
+    document.getElementById("outdoors-errors").innerHTML = error.message;
+  }
+}
+
+async function outdoorsApiCalls(items, areaIds) {
+  const searchItems = areaIds;
+  let parkName = [];
+  let parkDescription = [];
+  let parkLink = [];
+  for (let i = 0; i < items.length; i++) {
+    if (searchItems.includes(items[i].RecAreaID)) {
+      let outdoorsURL = items[i].RecAreaID;
+      let urlLink = await outdoorsUrlApiCall(outdoorsURL);
+      parkName.push(items[i].RecAreaName);
+      parkDescription.push(items[i].RecAreaDescription);
+      parkLink.push(urlLink);
+    } 
+  }
+  displayOutdoors(parkName, parkDescription, parkLink);
+}
+
+function displayOutdoors(parkName, parkDescription, parkLink) {
+  if (parkName.length === 0) {
+    $("#display-outdoors").text("This search returned no results; some park information may be out of date or unavailable.");
+  }
+  for (let j = 0; j < 6; j++) {
+    if (typeof parkName[j]!='undefined' && parkName[j]!=null){
+      let pointsHtml =
+      
+      `<div class="col my-3">
+        <div class="card mx-auto h-100" style="width: 18rem;">
+          
+          <div class="card-body">
+            <h5 class="card-title">${parkName[j]}</h5>
+            <hr>
+            <div class="scroll">
+              <p class="card-text">${parkDescription[j]}</p>
+            </div>
+          </div>
+          <div class="card-footer d-flex justify-content-center">
+            <a href=${parkLink[j]} class="btn inverted-button">Learn More</a>
+          </div>
+        </div>
+      </div>`;
+      $("#display-outdoors").append(pointsHtml);
+    }
+  }
+}
+
+function resetDisplay() {
+  $("#cityDisplay").empty();
+  $("#map-wrapper").empty();
+  $("#display-outdoors").empty();
+  $("#outdoors-errors").empty();
+  $("#movies").empty();
+  $("#navBar").hide();
+  $("#results-container").hide();
+  $("#container").show();
+}
+
+function createGeoJson(items) {
+  const features = items.map((item, i) => {
+    return {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          item.poi.coordinates.longitude,
+          item.poi.coordinates.latitude,
+        ],
+      },
+      properties: {
+        "marker-symbol": (i + 1).toString(),
+      },
+    };
+  });
+  
+  const geoJsonObject = {
+    type: "FeatureCollection",
+    features: features,
+  };
+
+  return encodeURIComponent(JSON.stringify(geoJsonObject));
+}
+
+function displayMovies(films) {
+  for (let i in films) {
+    let film = films[i];
+     
+    $("#movies").append(`
+    <div class="col my-3">
+      <div class="card mx-auto h-100" style="width: 18rem;">
+        <img class="card-img-top movie-image" src="${film.images.poster[1].medium.film_image}" alt="Card image cap">
+        <div class="card-body d-flex flex-column" id="${film.film_id}">
+          <h5 class="card-title">Movie: ${film.film_name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Release Date: ${film.release_dates[0].release_date}, Rated: ${film.age_rating[0].rating}</h6>
+          <p>${film.synopsis_long}</p>
+          <br>
+        </div>
+      </div>
+    </div>`);
+    Movies.getTheaters(film.film_id)
+      .then(result => {
+        for (let theaterId in result.cinemas) {
+          $(`#${film.film_id}`).append(`<p>${result.cinemas[theaterId].cinema_name}</p>
+          <p>${result.cinemas[theaterId].time}</p>`);
+        }
+      })
+      .catch(error => {
+        $(`#${film.film_id}`).append(`<p>Sorry, something went wrong and we couldn't fetch your nearest theaters showing this movie: ${error}</p>`);  
+      });
+  }
+}
+
+$(document).ready(function () {
+  $("#reset-button").click(function () {
+    resetDisplay();
+  });
+  $('.city').click(function () {
+    const cityName = this.id;
+    makeApiCall(cityName);
+  });
+  $("#outdoors").click(function () {
+    $("#display-outdoors, #outdoors-errors").empty();
+    const citySearch = $("#outdoors-search").val();
+    outdoorsIdCall(citySearch);
+  });
+  $(window).scroll(function () {
+    if ($(this).scrollTop()) {
+      $('#myBtn').fadeIn();
+    } else {
+      $('#myBtn').fadeOut();
+    }
+  });
+  $('#myBtn').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 50);
+  });
+  $("#movie-button").on("click", function() {
+    Movies.getMovies()
+      .then(result => {
+        displayMovies(result.films);
+      })
+      .catch(error => {
+        $("#movies").append(`<p>Sorry, something went wrong and we couldn't fetch your movies: ${error}</p>`);
+      });
+    $("#movie-button").hide();
+  });
+});
